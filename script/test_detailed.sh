f6b81fa45c265fcd4b3a58bdfd22649bf10667f5
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2025-05-05 19:55:02 -0700

    revised test scripts

--- script/test_detailed.sh
+++ script/test_detailed.sh
@@ -1,1 +1,1 @@
-colcon test --pytest-with-coverage --pytest-args --cov-report=term --event-handlers console_direct+
+colcon test --packages-select pwm_cltool --pytest-with-coverage --pytest-args --cov-report=term --event-handlers console_direct+
