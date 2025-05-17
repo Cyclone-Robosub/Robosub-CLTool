39d5e20e7db78fa5f00822762cf2b83ceffe8442
Dannykcw <106027442+Dannykcw@users.noreply.github.com>
2025-05-17 14:10:22 -0700

    removed crs_common

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,5 @@
+pushd lib/crs_common
+source script/build.sh      # this runs in the lib/crs_common workspace
+popd
 colcon build --packages-select pwm_cltool
 source install/setup.bash      # or setup.zsh
