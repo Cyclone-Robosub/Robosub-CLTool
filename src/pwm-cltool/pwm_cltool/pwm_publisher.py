6213061f42145c0d62ed459f81eb2b714221853b
zenyn <zenyn.ethridge@gmail.com>
2025-07-27 15:53:43 -0700

    merge

--- src/pwm-cltool/pwm_cltool/pwm_publisher.py
+++ src/pwm-cltool/pwm_cltool/pwm_publisher.py
@@ -1,4 +1,5 @@
 from std_msgs.msg import Int32MultiArray, Bool, Int64
+from crs_ros2_interfaces.msg import PwmCmd
 from rclpy.node import Node
 from rclpy.publisher import Publisher
 from typing import List
@@ -10,13 +11,13 @@ class Pwm_Publisher(Node):
     ROS 2 Node for publishing PWM control and override signals to various topics.
 
     This node is responsible for sending commands related to the pwm control tool (Cltool), including:
-    - PWM signal arrays to control thrusters or actuators.
-    - Duration values indicating how long a command should be applied.
+    - PwmCmd Messages which include:
+        - PWM signal arrays to control thrusters or actuators.
+        - Duration values indicating how long a command should be applied.
     - Boolean toggles for manual mode activation and emergency override.
 
     Topics Published:
-        - 'array_Cltool_topic' (Int32MultiArray): PWM command array.
-        - 'duration_Cltool_topic' (Int64): Duration of the command in seconds.
+        - 'array_Cltool_topic' (PwmCmd): PWM command array.
         - 'manual_toggle_switch' (Bool): Manual mode toggle signal.
         - 'manualOverride' (Bool): Manual override/emergency stop trigger.
     """
@@ -29,6 +30,7 @@ class Pwm_Publisher(Node):
         self.commandPublisher: Publisher = self.create_publisher(
             PwmCmd, "pwm_cmd_topic", 10
         )
+        
         self.ManualToggleSwitch: Publisher = self.create_publisher(
             Bool, "manual_toggle_switch", 3
         )
@@ -42,36 +44,24 @@ class Pwm_Publisher(Node):
 
         Args:
             pwm_array (List[int]): List of PWM values (typically 8 elements for 8 thrusters).
+            is_timed (bool): Bool for if supplied pwm values should be used for a specific duration or indefinetely.
+            pwm_duration (int): Duration to use the pwm values for. Should be ignored if is_time is true
         """
         msg = PwmCmd()
+        msg.pwm_flt = 0
+        msg.pwm_frt = 1
+        msg.pwm_rlt = 2
+        msg.pwm_rrt = 3
+        msg.pwm_flb = 10
+        msg.pwm_frb = 11
+        msg.pwm_rlb = 12
+        msg.pwm_rrb = 13
         
-        msg.pwm_flt = pwm_array[0]
-        msg.pwm_frt = pwm_array[1]
-        msg.pwm_rlt = pwm_array[2]
-        msg.pwm_rrt = pwm_array[3]
-        msg.pwm_flb = pwm_array[4]
-        msg.pwm_frb = pwm_array[5]
-        msg.pwm_rlb = pwm_array[6]
-        msg.pwm_rrb = pwm_array[7]
-        
+        msg.pwm_duration = pwm_duration
         msg.is_timed = is_timed
         
-        msg.duration = pwm_duration
-        
         self.commandPublisher.publish(msg)
-        
-        print(
-            f"pwm_flt = {msg.pwm_flt}\n"
-            f"pwm_frt = {msg.pwm_frt}\n"
-            f"pwm_rlt = {msg.pwm_rlt}\n"
-            f"pwm_rrt = {msg.pwm_rrt}\n"
-            f"pwm_flb = {msg.pwm_flb}\n"
-            f"pwm_frb = {msg.pwm_frb}\n"
-            f"pwm_rlb = {msg.pwm_rlb}\n"
-            f"pwm_rrb = {msg.pwm_rrb}"
-        )
-        print(f"is_timed = {msg.is_timed}")
-        print(f"duration = {msg.duration}")
+        print(msg)
 
     def publish_manual_switch(self, isManualEnabled: bool) -> None:
         """
