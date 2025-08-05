598982a7748bffb3da59ac17a5f7333f2bef19a0
zenyn <zenyn.ethridge@gmail.com>
2025-08-04 19:18:03 -0700

    commented out failing style tests

--- src/pwm-cltool/test/conftest.py
+++ src/pwm-cltool/test/conftest.py
@@ -1,23 +1,20 @@
 import pytest
 import rclpy
+
 from pwm_cltool.pwm_publisher import Pwm_Publisher
+
 
 @pytest.fixture(scope='session')
 def ros_init_and_shutdown():
-    """
-    Initialize rclpy once for the whole test session,
-    and shutdown when all tests are done.
-    """
+    """Initialize rclpy once for the whole test session and shutdown when all tests are done."""
     rclpy.init()
     yield
     rclpy.shutdown()
 
+
 @pytest.fixture(scope='function')
 def pwm_node():
-    """
-    For each test, create a fresh Pwm_Publisher node and
-    tear it down after the test finishes.
-    """
+    """Create a fresh Pwm_Publisher node for each test and tear it down after the test finishes."""
     node = Pwm_Publisher()
     yield node
     node.destroy_node()
